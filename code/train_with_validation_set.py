3211d89067fd1cd9f510b0fb25843ed81f841d7c
Andrew Craig <apc53c@gmail.com>
2017-12-06 09:04:41 +0000

    Now train_with_validation_set.py compares with/without early stopping. Added note to README.org.

--- code/train_with_validation_set.py
+++ code/train_with_validation_set.py
@@ -40,22 +40,27 @@ y = train_df.loc[:, 'target']
 fit_params = hyperparams['xgb']['fit']
 n_splits = 3
 kf = StratifiedKFold(n_splits=n_splits)
-scores = np.empty(n_splits)
-for i_fold, (fold_train_index, fold_test_index) in enumerate(kf.split(X, y)):
-    print("Training for fold " + str(i_fold + 1) + "/" + str(n_splits))
-    # Split train_index into train set and eval set for early stopping.
-    fold_X = X.loc[fold_train_index, :]
-    fold_y = y[fold_train_index]
-    X_train, X_test, y_train, y_test = train_test_split(fold_X, fold_y, test_size=0.2, stratify=fold_y) 
-    # eval_set: A list of (X, y) pairs to use as a validation set for early-stopping 
-    model.fit(X_train, y_train, eval_set=[(X_test, y_test)], **fit_params)
-    # Get best iteration based on eval_set.
-    sorted_iteration_scores = np.argsort(model.evals_result()['validation_0']['auc'])
-    best_round = sorted_iteration_scores[-1]
-    # Evaluate on test_index.
-    proba = model.predict_proba(X.loc[fold_test_index, :], ntree_limit=best_round)[:, 1]
-    y_true = y[fold_test_index]
-    scores[i_fold] = eval_gini(y_true, proba)
-# Report error.
-print('Gini score mean (standard deviation): ' + str(np.mean(scores)) + ' (' +  str(np.sqrt(np.var(scores))) + ')')
+# CV with and without early stopping.
+for early_stopping, early_stopping_string in zip([True, False], ['early stopping', 'no early stopping']):
+    scores = np.empty(n_splits)
+    for i_fold, (fold_train_index, fold_test_index) in enumerate(kf.split(X, y)):
+        print("Training for " + early_stopping_string + " fold " + str(i_fold + 1) + "/" + str(n_splits))
+        # Split train_index into train set and eval set for early stopping.
+        fold_X = X.loc[fold_train_index, :]
+        fold_y = y[fold_train_index]
+        X_train, X_test, y_train, y_test = train_test_split(fold_X, fold_y, test_size=0.2, stratify=fold_y) 
+        # eval_set: A list of (X, y) pairs to use as a validation set for early stopping 
+        model.fit(X_train, y_train, eval_set=[(X_test, y_test)], **fit_params)
+        if early_stopping:
+            # Get best iteration based on eval_set.
+            sorted_iteration_scores = np.argsort(model.evals_result()['validation_0']['auc'])
+            best_round = sorted_iteration_scores[-1]
+            # Evaluate on test_index.
+            proba = model.predict_proba(X.loc[fold_test_index, :], ntree_limit=best_round)[:, 1]
+        else:
+            proba = model.predict_proba(X.loc[fold_test_index, :])[:, 1]
+        y_true = y[fold_test_index]
+        scores[i_fold] = eval_gini(y_true, proba)
+    # Report error.
+    print('For ' + early_stopping_string + ', Gini score mean (standard deviation): ' + str(np.mean(scores)) + ' (' +  str(np.sqrt(np.var(scores))) + ')')
 
